4d4a944e02cdc078ee978aa3d42e15525dd96d69
jamilxt <jamilxt@gmail.com>
2020-09-14 05:59:54 +0600

    end 06_03 & begin 07_01

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -16,3 +17,41 @@ const functions = require("firebase-functions");
 // For a gmail account with 2-step verification enabled, you will need to use
 // an app password
 //  https://support.google.com/accounts/answer/185833
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+
+const mailTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+const APP_NAME = "The Grid Dashboard";
+
+async function sendWelcomeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <thegrid.manager@gmail.com`,
+    to: email,
+  };
+
+  mailOptions.subject = `Welcome to ${APP_NAME}`;
+  mailOptions.text = `Hey ${
+    displayName || ""
+  }! Welcome to the elite team of secret agents`;
+
+  await mailTransport.sendMail(mailOptions);
+
+  console.log("New welcome email sent to: ", email);
+
+  return null;
+}
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+  const email = user.email;
+  const displayName = user.displayName;
+
+  return sendWelcomeEmail(email, displayName);
+});
