e4a490b0078fc8637c9619ea057ad5c18de74b8a
jamilxt <jamilxt@gmail.com>
2020-09-14 05:40:57 +0600

    begin 06_03

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +7,12 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// for sending email:
+// 1. enable access to less secure apps:
+// https://www.google.com/settings/security/lesssecureapps
+// 2. Display Unlock Captcha:
+// https://accounts.google.com/DisplayUnlockCaptcha
+// For a gmail account with 2-step verification enabled, you will need to use
+// an app password
+//  https://support.google.com/accounts/answer/185833
