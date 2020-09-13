8f06fd0aaa25c60113f6ccd68c37b04adc4f09e0
jamilxt <jamilxt@gmail.com>
2020-09-14 04:44:57 +0600

    begin 05_06

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from "@angular/fire/firestore";
+import { UserProfile } from "../core/user-profile.model";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  selector: "app-users",
+  templateUrl: "./users.component.html",
+  styleUrls: ["./users.component.scss"],
 })
 export class UsersComponent implements OnInit {
+  private UsersCollection: AngularFirestoreCollection<UserProfile>;
+  users: Observable<UserProfile[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private angularFirestore: AngularFirestore) {
+    this.UsersCollection = angularFirestore.collection<UserProfile>("users");
+    this.users = this.UsersCollection.valueChanges();
   }
 
+  ngOnInit() {}
 }
